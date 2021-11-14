52d66ea15d29988d3b8dd3889d1e13186d466170
Ahrary <ahraary@gmail.com>
2021-11-15 02:03:11 +0500

    Using built-in nextjs link for home link

--- Layout/Header.js
+++ Layout/Header.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
+import Link from "next/link";
+
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
 
 function Header(props) {
   const { sections, title } = props;
@@ -21,8 +22,10 @@ function Header(props) {
           noWrap
           sx={{ flex: 1 }}
         >
-          <Link color="inherit" href="/" sx={{ textDecoration: "none" }}>
-            <b>{title}</b>
+          <Link href="/">
+            <a style={{ textDecoration: "none", color: "#333" }}>
+              <b>{title}</b>
+            </a>
           </Link>
         </Typography>
         <IconButton>
@@ -35,7 +38,7 @@ function Header(props) {
         sx={{ justifyContent: "space-between", overflowX: "auto" }}
       >
         {sections.map((section) => (
-          <Link
+          <Typography
             color="inherit"
             noWrap
             key={section.title}
@@ -43,8 +46,13 @@ function Header(props) {
             href={section.url}
             sx={{ p: 1, flexShrink: 0, textDecoration: "none" }}
           >
-            {section.title}
-          </Link>
+            <a
+              href={section.url}
+              style={{ textDecoration: "none", color: "#333" }}
+            >
+              {section.title}
+            </a>
+          </Typography>
         ))}
       </Toolbar>
     </>
